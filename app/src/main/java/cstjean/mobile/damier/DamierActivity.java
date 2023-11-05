d6d72bb8f7543dfdb8e7c1f79b6a5c49968945a7
EpicaZombi3-cstjean <2275509@cstjean.qc.ca>
2023-11-05 18:26:56 -0500

    Initial commit

---
+++ app/src/main/java/cstjean/mobile/damier/DamierActivity.java
@@ -0,0 +1,14 @@
+package cstjean.mobile.damier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DamierActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main);
+    }
+}
