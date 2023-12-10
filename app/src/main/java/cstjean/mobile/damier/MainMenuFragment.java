e1fe248929f4de420619a22f7657255d36b53277
MN2004 <2183438@cstjean.qc.ca>
2023-12-10 14:42:02 -0500

    Fait la vérification des noms
    ILS NE SONT PAS ENCORE DANS LE JEU

--- app/src/main/java/cstjean/mobile/damier/MainMenuFragment.java
+++ app/src/main/java/cstjean/mobile/damier/MainMenuFragment.java
@@ -2,15 +2,14 @@ package cstjean.mobile.damier;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
+import androidx.fragment.app.Fragment;
 import com.google.android.material.textfield.TextInputEditText;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,15 +17,21 @@ import com.google.android.material.textfield.TextInputEditText;
  * create an instance of this fragment.
  */
 public class MainMenuFragment extends Fragment {
+    /**
+     * Le texte pour écrire le nom du joueur blanc.
+     */
     private TextInputEditText joueur1;
+    /**
+     * Le texte pour écrire le nom du joueur noir.
+     */
     private TextInputEditText joueur2;
-    private Button commencer;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
-    //TODO mettre une logique pour que le texte ne soit pas vide
+    // TODO mettre une logique pour que le texte ne soit pas vide
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -34,11 +39,20 @@ public class MainMenuFragment extends Fragment {
 
         joueur1 = view.findViewById(R.id.text_joueur1);
         joueur2 = view.findViewById(R.id.text_joueur2);
-        commencer = view.findViewById(R.id.btn_jouer);
+        Button commencer = view.findViewById(R.id.btn_jouer);
 
         commencer.setOnClickListener(v -> {
-            Intent intent = new Intent(getActivity(), DamierActivity.class);
-            startActivity(intent);
+            String nom1 = (Objects.requireNonNull(joueur1.getText())).toString();
+            String nom2 = (Objects.requireNonNull(joueur2.getText())).toString();
+
+            nom1 = nom1.replace(" ", "");
+            nom2 = nom2.replace(" ", "");
+            if (!TextUtils.isEmpty(nom1) && !TextUtils.isEmpty(nom2)) {
+                Intent intent = new Intent(getActivity(), DamierActivity.class);
+                intent.putExtra("nomJoueur1", nom1);
+                intent.putExtra("nomJoueur2", nom2);
+                startActivity(intent);
+            }
         });
 
         return view;
